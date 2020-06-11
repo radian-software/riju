8246ab32d0b4435f234c81eb93c89374f17493a7
Radon Rosborough <radon.neon@gmail.com>
2020-06-11 15:06:26 -0600

    Fix setuid bit on riju-install

--- scripts/install-scripts.bash
+++ scripts/install-scripts.bash
@@ -8,6 +8,6 @@ cp scripts/certbot-pre.bash /etc/letsencrypt/renewal-hooks/pre/riju
 cp scripts/certbot-post.bash /etc/letsencrypt/renewal-hooks/post/riju
 cp scripts/install.py /usr/bin/riju-install
 chgrp deploy /usr/bin/riju-install
-chmod u=rwxs,g=rx,o= /usr/bin/riju-install
+chmod u=rwxs,g=rxs,o= /usr/bin/riju-install
 
 systemctl daemon-reload
