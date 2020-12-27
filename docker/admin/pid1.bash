1725811aede71634326c462727e23975df0242cf
Radon Rosborough <radon.neon@gmail.com>
2020-12-27 13:21:58 -0800

    Fix warning from sudo

--- docker/admin/pid1.bash
+++ docker/admin/pid1.bash
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+tee -a /etc/hosts >/dev/null <<< "127.0.0.1 admin"
+
 groupadd -g "$(stat -c %g "$PWD")" -o -p '!' -r riju
 useradd -u "$(stat -c %u "$PWD")" -g "$(stat -c %g "$PWD")" -o -p '!' -m -N -l -s /usr/bin/bash -G sudo riju
 
