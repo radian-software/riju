2d95bb12c344fe8ed33468beb4174cb2b542e5a6
Radon Rosborough <radon@intuitiveexplanations.com>
2023-04-22 12:44:44 -0700

    Port rest of manifests to DigitalOcean

--- env.yaml.bash
+++ env.yaml.bash
@@ -10,30 +10,20 @@ proxy_password="$(pwgen -s 20 1)"
 cat <<EOF
 networking:
   domain: riju.example.com  # FIXME
-  ip: x.y.z.w  # FIXME
 
-s3:
-  accessKeyId: fixme
-  accessKeySecret: fixme
-  bucket: fixme
-  region: fixme
-  endpoint: fixme.digitaloceanspaces.com
-
-contact:
-  letsEncryptEmail: ops@example.com  # FIXME
-  letsEncryptProductionEnabled: false
-
-metallb:
-  secretkey: "$(pwgen -s 256 1)"
+letsEncrypt:
+  contactEmail: ops@example.com  # FIXME
 
 registry:
   password: "${registry_password}"
   htpasswd: "$(htpasswd -nbB admin "${registry_password}")"
   httpSecret: "$(pwgen -s 16 1)"
-
-minio:
-  accessKey: "$(head -c16 /dev/urandom | xxd -p)"
-  secretKey: "$(head -c16 /dev/urandom | xxd -p)"
+  s3:
+    accessKeyId: fixme
+    accessKeySecret: fixme
+    bucket: fixme
+    region: fixme
+    endpoint: fixme.digitaloceanspaces.com
 
 proxy:
   password: "${proxy_password}"
